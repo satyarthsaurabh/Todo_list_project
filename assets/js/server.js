fa58b452140787488d8eb090fe8b31e5b0f876cb
satyarthsaurabh <satyarthsaurabh1996@gmail.com>
2020-04-27 02:00:29 +0530

    todo list

---
+++ assets/js/server.js
@@ -0,0 +1,26 @@
+// alert("jsdflaj");
+
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+        $(this).parent().fadeOut(500, function(){
+            $(this).remove();
+        });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    // console.log("key press");
+
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
